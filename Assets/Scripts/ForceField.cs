9235c2469e5369d218d85227f7754900c1476ab8
lxwooxy <69057359+lxwooxy@users.noreply.github.com>
2020-08-05 22:43:26 -0400

    Initial Commit

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    //Variables
+    [Tooltip("Should the force field affect the player?")]
+    public bool affectsPlayer = true;
+
+    [Tooltip("Should the force field affect Rigidbodies?")]
+    public bool affectsRigidbodies = true;
+
+    [Tooltip("Method of applying force.")]
+    public ForceMode forceMode;
+
+    [Tooltip("Amount of force applied.")]
+    public Vector3 force;
+
+    [Tooltip("Should the force be applied in world space or local spave relative to this Transform's facing?")]
+    public Space forceSpace = Space.World;
+
+    //Get the force in world space.
+    public Vector3 ForceInWorldSpace
+    {
+        get
+        {
+            //If it's world-space we can just return'force' as is:
+            if (forceSpace == Space.World)
+                return force;
+
+            //If it's local space, we use our transform to convert 'force' from local to world space:
+            else
+                return transform.TransformDirection(force);
+        }
+    }
+
+    //Methods
+    void OnColliderTouched(Collider other)
+    {
+        //If we affect the player,,
+        if (affectsPlayer)
+        {
+            //Check for a player component on the other collider's GameObject:
+            var player = other.GetComponent<Player>();
+
+            //If we found one, call AddVelocity:
+            if (player != null)
+            {
+                //If the force mode is a constant push mode, use TIme.deltaTIme to make the force "per second".
+                if (forceMode == ForceMode.Force || forceMode == ForceMode.Acceleration)
+                    player.AddVelocity(ForceInWorldSpace * Time.deltaTime);
+                else //Otherwise, use teh force as is.
+                    player.AddVelocity(ForceInWorldSpace);
+            }
+        }
+        //If we affect Rigidbodies,
+        if (affectsRigidbodies)
+        {
+            //Check for a Rigidbody component on the other collider's GameObject:
+            var rb = other.GetComponent<Rigidbody>();
+
+            //If we found one, call AddForce:
+            if (rb != null)
+                rb.AddForce(ForceInWorldSpace,forceMode);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Impulse and VelocityChange modes will apply force only when the trigger is first entered.
+        if(forceMode == ForceMode.Impulse || forceMode == ForceMode.VelocityChange)
+            OnColliderTouched(other);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //Acceleration and Force modes will apply force constantly as long as the collision stays in contact.
+        if (forceMode == ForceMode.Acceleration || forceMode == ForceMode.Force)
+            OnColliderTouched(other);
+    }
+}
